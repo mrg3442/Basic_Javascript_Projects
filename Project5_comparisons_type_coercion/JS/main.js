53bb9e10619012bfff2571085848535914c93903
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-03-23 02:13:07 -0400

    Learned how to use boolean operators

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,34 @@
+document.write(typeof 3);//Returns number
+document.write(typeof "Word");//returns string
+document.write(5 + " Hello");//combines num and string
+alert(10348908484E20000 + 10);//Displays infinity
+document.write("<br>" + 0/0);//Displays NAN
+document.write("<br>" + -1300E89999*12032E999900000);//Displays negative infinity
+document.write("<br>");//adds a break
+document.write(8000>1);//displays true
+document.write("<br>");//adds a break
+document.write(80000<1);//displays false
+console.log(3*7);//displays 21 in console
+console.log(2>3);//displays false in console
+document.write("<br>");//adds a break
+document.write(16==18);//displays false
+document.write("<br>");//adds a break
+document.write(16==16);//displays true
+var str = "OK";
+var str2 = "OK";
+var num = "0";
+var num2 = 0;
+document.write("<br>");//adds a break
+document.write(str===str2);//returns true
+document.write("<br>");//adds a break
+document.write(num2===num);//returns false compares a int to a str
+document.write("<br>");//adds a break
+document.write(23>5 && 24>4);//returns true using and op
+document.write("<br>");//adds a break
+document.write(23>4 || 25<4);//returns true using or op
+var test = 3;
+document.write("<br>");//adds a break
+document.write("We are at the not op next");
+document.write("<br>");//adds a break
+document.write(test != 3);//returns false
+document.write(test != 4);//returns true
