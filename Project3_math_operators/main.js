c30b37c71dc787657f3eba1b161e194e2ce8750e
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-03-22 21:25:48 -0400

    Learned various math operations in JS

---
+++ Project3_math_operators/main.js
@@ -0,0 +1,48 @@
+function multiFunction(){
+    var num = 3*3;
+    document.getElementById("math").innerHTML = num + " This is multiply";
+}
+
+function subFunction(){
+    var num=8-2;
+    document.getElementById("sub").innerHTML = num + " This is subtraction";
+}
+
+function divFunction(){
+    var num=8/2;
+    document.getElementById("div").innerHTML = num + " This is division";
+}
+
+function addFunction(){
+    var num=8+2;
+    document.getElementById("add").innerHTML = num + " This is addition";
+}
+
+function multipleFunction() {
+    var num = 18/2+3-3;
+    document.getElementById("multiple").innerHTML = num + " This is all four math operations.";
+}
+
+function remFunction() {
+    var num= 14%6;
+    document.getElementById("rem").innerHTML = num + " This is the remainder of 14 divided by 6."
+}
+
+function negFunction() {
+    var num = 8;
+    document.getElementById("neg").innerHTML = -num + " This is the negation of 8";
+}
+
+function incFunction() {
+    var num = 8;
+    num++;
+    document.getElementById("inc").innerHTML = num + " This is an increase of 1 from 8";
+}
+
+function decFunction() {
+    var num = 8;
+    num--;
+    document.getElementById("dec").innerHTML = num + " This is a decrease of 1 from 8";
+}
+
+window.alert(Math.random() * 1000);
