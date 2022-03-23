1d2619995da958f0b515936d822c312e2941c72b
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-03-23 01:46:54 -0400

    Update main.js

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -2,9 +2,9 @@ var car = {
     Make:"Ford",
     Model:"Bronco",
     Year:"2022"
-}
+} //dictionary about various car specs
 
 function carInfo() {
-    delete car.Model;
-    document.getElementById("Dictionary").innerHTML = car.Model;
+    delete car.Model;//deletes model so that we get undefined when called
+    document.getElementById("Dictionary").innerHTML = car.Model;//calls the model from car dictionary and updates the <p> with corresponding tag
 }
