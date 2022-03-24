47ea2b55363e4cad766f627cab21c2c85ab42085
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-03-24 04:16:11 -0400

    if and else and else if practice

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,6 +1,22 @@
 var str = "ok";
 
 function intentional() {
-    var num = 9;
-    document.getElementById("pop").innerHTML = num;
+   if(new Date().getHours() > 20 || new Date().getHours() < 5) {
+    document.getElementById("pop").innerHTML = "Have a goodnight!";
+   }
+}
+
+function ifStat() {
+    if(new Date().getHours()  < 4) {
+        document.write("<br> Why are you up so late?");
+    }
+}
+
+function ifFunc(age) {
+    var fg = parseInt(age);
+    if(fg >= 18){
+        document.getElementById("req").innerHTML = "You are an adult!";
+    }else{
+        document.getElementById("req").innerHTML = "You are still a minor.";
+    }
 }
