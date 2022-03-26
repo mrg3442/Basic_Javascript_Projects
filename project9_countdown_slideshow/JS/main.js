5ae71dea8e60fd822685a83a5da0539d0e6747ca
mrg3442 <101129274+mrg3442@users.noreply.github.com>
2022-03-26 03:19:31 -0400

    added a countdown to slide change

--- project9_countdown_slideshow/JS/main.js
+++ project9_countdown_slideshow/JS/main.js
@@ -1,7 +1,25 @@
 let slideIndex = 0;
 showSlides();
 
+
 function showSlides() {
+
+  function countdown() {
+    var seconds = 6;
+    
+    function tick() {
+      seconds = seconds -1;
+      timer.innerHTML = seconds;
+      var time = setTimeout(tick, 1000)
+      if (seconds == -1) {
+        clearTimeout(time);
+        timer.innerHTML = "";
+      }
+    }
+    tick();
+  }
+  countdown();
+
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
@@ -17,3 +35,4 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 6000); // Change image every 6 seconds
 }
+
